441194f63772b0fae13340a4a3e2fbe4d82d386c
ByKaS12 <67284495+ByKaS12@users.noreply.github.com>
2021-05-21 00:52:52 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab_4_5_Logic_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("19. Перевести число из десятичной системы счисления в систему с основанием N, где N<10, N>1.");
+            Console.WriteLine("18. Вычислить количество четных элементов на заданном интервале.");
+            Console.WriteLine("11. Найти цифры в десятичной записи двузначного натурального числа.");
+            Console.WriteLine("Введите номер задания!");
+            int Task = Convert.ToInt32(Console.ReadLine());
+
+            switch (Task)
+            {
+                case 19:
+                    {
+                        Console.WriteLine("Введите десятичное число !");
+                        int Num = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Введите  новое основание числа!");
+                        int N = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Полученное число = " + MockBaseConvertation.DecimalToArbitrarySystem(Num, N) + " " + "с основанием " + N);
+                        break;
+                    }  
+                case 18:
+                    {
+                        
+                        Console.WriteLine("Да. Сделать заданный интервал случайным !");
+                        Console.WriteLine("Нет. Вписать интервал самим !");
+                        Console.WriteLine("Подтвердите выбор !");
+                        string t = Console.ReadLine();
+                        switch (t)
+                        {
+                            case "Да":
+                                {
+                                    Console.WriteLine("Введите количество элементов  !");
+                                    int Num = Convert.ToInt32(Console.ReadLine());
+                                    int[] mass = new int[Num];
+                                    for (int i = 0; i < mass.Length; i++)
+                                    {
+                                        mass[i] = new Random().Next(0, 1000);
+                                        Console.Write(mass[i]);
+                                        Console.Write(" ");
+                                    }
+                                    Console.WriteLine();
+                                    Console.WriteLine("Сумма нечетных чисел случайного интервала = " + Task_18.FillMass(mass));
+                                    break;
+                                }
+                            case "Нет":
+                                {
+                                    Console.WriteLine("Введите количество элементов  !");
+                                    int Num = Convert.ToInt32(Console.ReadLine());
+                                    int[] mass = new int[Num];
+                                    for (int i = 0; i < mass.Length; i++)
+                                    {
+                                        Console.WriteLine("Введите число");
+                                        int elem = Convert.ToInt32(Console.ReadLine());
+                                        mass[i] = elem;
+                                    }
+                                    Console.WriteLine();
+                                    for (int i = 0; i < mass.Length; i++)
+                                    {
+                                        Console.Write(mass[i]);
+                                        Console.Write(" ");
+                                    }
+                                    Console.WriteLine();
+                                    Console.WriteLine("Сумма нечетных чисел заданного интервала = " + Task_18.FillMass(mass));
+                                    break;
+                               
+                                }
+                            default:
+                                break;
+                        } ;
+                        
+                        break;
+                    } 
+                case 11:
+                    {
+                        Console.WriteLine("Введите десятичное число !");
+                        string Num = Console.ReadLine();
+                        Console.WriteLine("Найденные цифры:");
+                        Console.WriteLine(Task_11.Result(Num));
+                        break;
+                    }
+
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ Tasks.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_4_5_Logic_Program
+{
+
+    public static class Task_18
+    {
+       public static int FillMass(int[] mass)
+        {
+            int count = 0;
+            for (int i = 0; i < mass.Length; i++)
+            {
+                if (mass[i] % 2 == 0) count++;
+
+            }
+            return count;
+        }
+    }
+    public static class Task_11
+    {
+        public static string Result(string num)
+        {
+            
+            string copy = num.Trim(' ');
+            string result = "";
+            string numbers = "0123456789";
+            char[] chopper = copy.ToCharArray();
+            for (int i = 0; i < chopper.Length; i++)
+            {
+                if (numbers.Contains(chopper[i]) == true) { result += chopper[i]; result += " "; }
+            }
+            return result;
+        }
+
+    }
+    public static class MockBaseConvertation
+        {
+            public static string DecimalToArbitrarySystem(long decimalNumber, int radix)
+            {
+                const int BitsInLong = 64;
+                const string Digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+                if (radix < 2 || radix > Digits.Length)
+                    throw new ArgumentException("The radix must be >= 2 and <= " + Digits.Length.ToString());
+
+                if (decimalNumber == 0)
+                    return "0";
+
+                int index = BitsInLong - 1;
+                long currentNumber = Math.Abs(decimalNumber);
+                char[] charArray = new char[BitsInLong];
+
+                while (currentNumber != 0)
+                {
+                    int remainder = (int)(currentNumber % radix);
+                    charArray[index--] = Digits[remainder];
+                    currentNumber = currentNumber / radix;
+                }
+
+                string result = new String(charArray, index + 1, BitsInLong - index - 1);
+                if (decimalNumber < 0)
+                {
+                    result = "-" + result;
+                }
+
+                return result;
+            }
+            public static long ArbitraryToDecimalSystem(string number, int radix)
+            {
+                const string Digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+                if (radix < 2 || radix > Digits.Length)
+                    throw new ArgumentException("The radix must be >= 2 and <= " +
+                        Digits.Length.ToString());
+
+                if (String.IsNullOrEmpty(number))
+                    return 0;
+                number = number.ToUpperInvariant();
+
+                long result = 0;
+                long multiplier = 1;
+                for (int i = number.Length - 1; i >= 0; i--)
+                {
+                    char c = number[i];
+                    if (i == 0 && c == '-')
+                    {
+                        result = -result;
+                        break;
+                    }
+
+                    int digit = Digits.IndexOf(c);
+                    if (digit == -1)
+                        throw new ArgumentException(
+                            "Invalid character in the arbitrary numeral system number",
+                            "number");
+
+                    result += digit * multiplier;
+                    multiplier *= radix;
+                }
+
+                return result;
+            }
+        }
+    }
+
